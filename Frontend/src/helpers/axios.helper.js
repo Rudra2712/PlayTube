a83e6ab6a5df3676425c3e3ead247fc7bf2ab86f
Rudra <rudramoradiya2712@gmail.com>
2025-10-11 16:01:16 +0530

    changed on axios.helper.js

--- Frontend/src/helpers/axios.helper.js
+++ Frontend/src/helpers/axios.helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "https://play-tube-backend-flax.vercel.app/api/v1";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "https://playtube-bf9m.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
